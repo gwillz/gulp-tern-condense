5ee580e6f6205bbdf6e552e79e13e9299c3e49bd
gwillz <gwillz@internode.on.et>
2017-08-22 20:59:48 +0930

    ensure unique input arrays, added alternate server option

--- src/index.js
+++ src/index.js
@@ -5,37 +5,44 @@ var readonly = require('read-only-stream')
 var tern = require('tern')
 var condense = require('tern/lib/condense')
 
-module.exports = function(files, options) {
+module.exports = function(options) {
+    var files = typeof options.files === 'string' ? [options.files] : files
+    
     options = Object.assign({
         plugins: ['es_modules'],
         defs: [],
-        name: files,
+        name: options.files[0],
         options: {spans: true},
     }, options)
     
-    var origins = glob.sync(files)
+    var origins = files
+    .reduce((sum, f) => sum.concat(glob.sync(f)), [])
+    .filter((v, i, self) => !self.slice(0, i).includes(i))
     
-    var defs = ['ecmascript'].map((name) => (
+    var defs = ['ecmascript']
+    .concat(options.defs)
+    .filter((v, i, self) => !self.slice(0, i).includes(i))
+    .map(name => (
         JSON.parse(fs.readFileSync(require.resolve(`tern/defs/${name}.json`), 'utf8'))
-    )).concat(options.defs)
+    ))
     
     var plugins = {};
-    options.plugins.forEach((name) => {
+    options.plugins.forEach(name => {
         plugins[name] = fs.readFileSync(require.resolve(`tern/plugin/${name}.js`), 'utf8')
     })
     
-    var server = new tern.Server({
+    var server = options.options.server || new tern.Server({
         defs: defs,
         plugins: plugins
     })
     
-    origins.forEach((f) => {
+    origins.forEach(f => {
         server.addFile(f, fs.readFileSync(f, 'utf8'))
     })
     
     var stream = through();
-    server.flush(function(err) {
-        if (err) throw err
+    server.flush(err => {
+        if (err) throw new Error(`gulp-tern-condense: {error.message}`)
         
         var code = condense.condense(origins, options.name, options.options)
         stream.write(JSON.stringify(code, null, 2))
